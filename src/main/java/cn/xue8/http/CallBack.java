ca5c8fa7e920264f9e8345a6d5d0141c38207e0f
xue8 <3788837@qq.com>
2019-04-12 18:26:09 +0800

    “remove旧版

--- src/main/java/cn/xue8/http/CallBack.java
+++
@@ -1,7 +0,0 @@
-package cn.xue8.http;
-
-import cn.xue8.http.executor.Call;
-
-public interface CallBack {
-    void onResponse(Response response);
-}
--- src/main/java/cn/xue8/http/pipeline/Interceptor.java
+++
@@ -1,7 +0,0 @@
-package cn.xue8.http.pipeline;
-
-import cn.xue8.http.Response;
-
-public interface Interceptor {
-    Response proceed(Response response);
-}
